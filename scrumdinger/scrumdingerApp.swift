5b96d7a0af8ec33f200bc750acdb426c57b8f36e
Oleksandr Radetskyi <radetskyi@outloo.com>
2022-11-10 01:04:59 +0000

    Initial Commit

---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Oleksandr Radetskyi on 09.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
